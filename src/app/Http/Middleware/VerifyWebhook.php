f4c5ec1c5f179676f7d31de6ef1a35deaecfea9d
andrew <hoangminh.it4u@gmail.com>
2020-03-27 08:33:16 +0700

    Udpated webhooks

---
+++ src/app/Http/Middleware/VerifyWebhook.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class VerifyWebhook
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $hmac = request()->header('x-shopify-hmac-sha256') ?: '';
+        $shop = request()->header('x-shopify-shop-domain');
+        $data = request()->getContent();
+
+        // From https://help.shopify.com/api/getting-started/webhooks#verify-webhook
+        $hmacLocal = base64_encode(hash_hmac('sha256', $data, env('SHARED_SECRET'), true));
+        if (!hash_equals($hmac, $hmacLocal) || empty($shop)) {
+            abort(401, 'Invalid webhook signature');
+        }
+
+        return $next($request);
+    }
+}
